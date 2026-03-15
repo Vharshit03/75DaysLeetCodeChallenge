1608115193dc72a928053f992142b377d8dc8abe
Harshit Vashisht <150457097+Vharshit03@users.noreply.github.com>
2026-03-15 17:28:35 +0530

    Time: 0 ms (100%), Space: 10.7 MB (52.41%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        
+        if(n==1)
+        return nums[0];
+
+        vector<int>dp1(n+2,-1);
+        vector<int>dp2(n+2,-1);
+        dp1[n-1]=dp1[n]=dp2[n+1]=dp2[n]=0;
+
+        for(int i=n-2;i>-1;i--)
+        dp1[i]= max(nums[i]+dp1[i+2],dp1[i+1]);
+
+        for(int i=n-1;i>0;i--)
+        dp2[i]= max(nums[i]+dp2[i+2],dp2[i+1]);
+
+        return max(dp1[0],dp2[1]);
+    }
+};
