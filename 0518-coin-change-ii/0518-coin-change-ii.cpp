244a86bc149f79511373d0a22d4d0fa0bc0ea343
Harshit Vashisht <150457097+Vharshit03@users.noreply.github.com>
2026-03-24 00:28:08 +0530

    Time: 4 ms (93.13%), Space: 10.5 MB (78.38%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-    int find(int index,int amount,vector<int>&coins,vector<vector<int>>&dp){
-
-        if(amount==0)
-        return 1;
-
-        if(index<0)
-        return 0;
-
-        if(dp[index][amount]!=-1)
-        return dp[index][amount];
-
-        if(coins[index]>amount)
-        return dp[index][amount] = find(index-1,amount,coins,dp);
-        else
-        return dp[index][amount] = find(index,amount-coins[index],coins,dp)+find(index-1,amount,coins,dp);
-    }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return find(n-1,amount,coins,dp);
+        vector<unsigned long long>dp(amount+1,0);
+        dp[0]=1;
+
+        for(int i=1;i<=n;i++){
+            for(int j=coins[i-1];j<=amount;j++)
+            dp[j] += dp[j-coins[i-1]];
+        }
+
+        return dp[amount];
     }
 };
