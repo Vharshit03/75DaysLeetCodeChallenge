e54f7dc1fa3204afe12493ce9700ba93b3c787e2
Harshit Vashisht <150457097+Vharshit03@users.noreply.github.com>
2026-04-27 22:52:51 +0530

    Time: 431 ms (52.16%), Space: 578.4 MB (39.67%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -1,15 +1,40 @@
 class TrieNode {
-public:
-   unordered_map<char, TrieNode*> children;
-   bool isWord;
+    TrieNode* links[26];
+    bool flag;
 
-   TrieNode(){
-    isWord = false;
-   }
+    public:
+    bool checkNode(char ch){
+        return links[ch-'a']!=NULL;
+    }
+
+    bool checkNode(int index){
+        return links[index]!=NULL;
+    }
+
+    void setNode(char ch,TrieNode* node){
+        links[ch-'a'] = node;
+    }
+
+    TrieNode* getNode(char ch){
+        return links[ch-'a'];
+    }
+
+    TrieNode* get(int index){
+        return links[index];
+    }
+
+    void setEnd(){
+        flag = true;
+    }
+
+    bool isEnd(){
+        return flag;
+    }
+   
 };
+
 class WordDictionary {
-private:
-   TrieNode* root;
+private: TrieNode* root;
 public:
     WordDictionary() {
         root = new TrieNode();
@@ -17,42 +42,46 @@ public:
     
     void addWord(string word) {
         TrieNode* node = root;
-        for(int i=0;i<word.length();i++){
-            char c = word[i];
-            if(node->children.find(c)== node->children.end())
-            node->children[c] = new TrieNode();
+        for(int i=0;i<word.size(); i++){
+            if(!node->checkNode(word[i])){
+                node->setNode(word[i],new TrieNode());
+            }
 
-            node = node->children[c];
+            node = node->getNode(word[i]);
         }
-        node->isWord = true;
+
+        node->setEnd();
+    }
+
+    bool searchUtil(TrieNode* root,string word){
+
+        TrieNode* node = root;
+        for(int i=0;i<word.size(); i++){
+
+            char ch = word[i];
+
+            if(ch=='.'){
+                for(int j=0;j<26;j++){
+                    
+                    if(node->checkNode(j)){
+                        if(searchUtil(node->get(j),word.substr(i+1)))
+                        return true;
+                    }
+                }
+                return false;
+            }
+            else if(!node->checkNode(ch))
+            return false;
+
+            node = node->getNode(ch);
+        }
+
+        return node!=NULL && node->isEnd();
     }
     
     bool search(string word) {
-        return searchHelper(root,word,0);
-    }
-    bool searchHelper(TrieNode* node,string& word,int index){
 
-        if(index == word.length()){
-            return node->isWord;
-        }
-        char c = word[index];
-        if(c=='.'){
-
-            for(auto& p:node->children){
-                if(searchHelper(p.second,word,index+1)){
-                    return true;
-                }
-            }
-            return false;
-
-        }else{
-
-            if(node->children.find(c)==node->children.end()) {
-                return false;
-            }
-            TrieNode* child = node->children[c];
-            return searchHelper(child,word,index+1);
-        }
+        return searchUtil(root,word);
     }
 };
 
