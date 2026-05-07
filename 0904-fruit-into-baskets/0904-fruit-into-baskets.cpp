9dbdc72880c8673b85a08a9198ac745b8f47698a
Harshit Vashisht <150457097+Vharshit03@users.noreply.github.com>
2026-05-07 23:30:14 +0530

    Time: 45 ms (49.95%), Space: 85 MB (34.4%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -8,14 +8,16 @@ public:
         while(r<n){
             mp[fruits[r]]++;
 
-            while(mp.size()>2){
+            if(mp.size()>2){
                 mp[fruits[l]]--;
                 if(mp[fruits[l]]==0) mp.erase(fruits[l]);
 
                 l++;
             }
 
+            if(mp.size()<=2)
             maxlen = max(maxlen,r-l+1);
+            
             r++;
         }
 
