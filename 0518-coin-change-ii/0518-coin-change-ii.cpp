820dbf47461e16ecdd26333f8989663465172be2
Harshit Vashisht <150457097+Vharshit03@users.noreply.github.com>
2026-05-07 21:04:58 +0530

    Time: 36 ms (36.25%), Space: 61.8 MB (6.07%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    int find(int ind,int amt,vector<int>&coins,vector<vector<int>> &dp){
-
-        if(amt==0)
-        return 1;
-
-        if(ind==0)
-        return 0;
-
-        if(dp[ind][amt]!=-1)
-        return dp[ind][amt];
-
-        int take = 0;
-
-        if(coins[ind-1]<=amt)
-        take = find(ind,amt-coins[ind-1],coins,dp);
-
-        int noTake = find(ind-1,amt,coins,dp);
-
-        return dp[ind][amt] =  take + noTake;
-    }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        return find(coins.size(),amount,coins,dp);
+        vector<vector<unsigned long long>>dp(n+1,vector<unsigned long long>(amount+1,0));
+
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(coins[i-1]>j)
+                dp[i][j]=dp[i-1][j];
+                else
+                dp[i][j]=dp[i][j-coins[i-1]] + dp[i-1][j];
+            }
+        }
+
+        return dp[n][amount];
     }
 };
