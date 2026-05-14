74e5d90d70cb40337e2265c5eb0d1093204fef44
Harshit Vashisht <150457097+Vharshit03@users.noreply.github.com>
2026-05-14 21:23:42 +0530

    Time: 7 ms (69.79%), Space: 22.9 MB (76.77%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+         int start,end=0,mid,ans,n= piles.size();
+        long long sum=0; // bananas
+        for(int i=0; i<n; i++)
+        {
+            sum+=piles[i];
+            end = max(end,piles[i]);
+        }
+        start = sum/h;
+        
+        if(!start)
+        start=1;
+
+        while(start<=end)
+        {
+            mid = end + (start-end)/2;
+            int total_time =0;
+            for(int i=0;i<n;i++)
+            {
+                total_time+=piles[i]/mid;
+                if(piles[i]%mid)
+                total_time++;
+            }
+            if(total_time>h)
+            {
+                start=mid+1;
+            }
+            else
+            {
+                ans=mid;
+                end=mid-1;
+            }
+        }
+
+        return ans;
+    }
+};
