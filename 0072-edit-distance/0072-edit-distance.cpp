0a4a4f9eea542b21825228f9b90b82fe4cd5d90b
Harshit Vashisht <150457097+Vharshit03@users.noreply.github.com>
2026-05-20 19:50:28 +0530

    Time: 0 ms (100%), Space: 10.3 MB (98.76%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -5,18 +5,25 @@ public:
         int n = word1.size();
         int m = word2.size();
 
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        for(int j=0;j<=m;j++) dp[0][j] = j;
-        for(int i=0;i<=n;i++) dp[i][0] = i;
+        vector<int>dp(m+1,0);
+        for(int j=0;j<=m;j++) dp[j] = j;
+
+        int prev,curr;
 
         for(int i=1;i<=n;i++){
+            prev = dp[0];
+            dp[0] = i;
             for(int j=1;j<=m;j++){
-                if(word1[i-1]==word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                curr = dp[j];
+                if(word1[i-1]==word2[j-1]) dp[j] = prev;
                 else
-                dp[i][j] = 1 + min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
+                dp[j] = 1 + min(dp[j-1],min(dp[j],prev));
+
+                prev = curr;
             }
+            
         }
 
-        return dp[n][m];
+        return dp[m];
     }
 };
