e5d523c42dea734e27e04348c8e2e13837e0ad6b
Harshit Vashisht <150457097+Vharshit03@users.noreply.github.com>
2026-05-29 06:07:55 +0530

    Time: 27 ms (55.82%), Space: 23.3 MB (25.98%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,54 +1,51 @@
 class Solution {
 public:
     bool check(int i,int j,int row,int col){
-        return i>-1&&i<row && j>-1&&j<col; //edge case outergrid cases
+        return i>-1 && i<row && j>-1 && j<col;
     }
-    void Bfs(vector<vector<char>>&grid,int i,int j){
-        
-        int m = grid.size();
-        int n = grid[0].size();
+    void Bfs(vector<vector<char>>& grid,int i,int j,int n,int m){
+
         queue<pair<int,int>> q;
+
         int row[4] = {-1,1,0,0};
         int col[4] = {0,0,-1,1};
+
         q.push({i,j});
 
-        
         while(!q.empty()){
-
-            i = q.front().first;
-            j = q.front().second;
+            int x = q.front().first;
+            int y = q.front().second;
             q.pop();
 
             for(int k=0;k<4;k++){
+                int xi = x+row[k];
+                int yj = y+col[k];
 
-                if(check(i+row[k], j+col[k], m, n) && grid[i+row[k]][j+col[k]]=='1'){
-                    
-                    grid[i+row[k]][j+col[k]]='0';
-                    q.push({i+row[k],j+col[k]});
+                if(check(xi,yj,n,m) && grid[xi][yj]=='1'){
+                    q.push({xi,yj});
+                    grid[xi][yj]='0';
                 }
             }
 
         }
     }
     int numIslands(vector<vector<char>>& grid) {
+        
+        int cnt=0;
+        int n = grid.size();
+        int m = grid[0].size();
 
-        int m = grid.size();
-        int n = grid[0].size();
-        int count=0;
-
-        for(int i=0;i<m;i++){
-
-            for(int j=0; j<n; j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
 
                 if(grid[i][j]=='1'){
-                    count++;
-                    grid[i][j]='0';
-                    Bfs(grid,i,j);      
+                    grid[i][j]=0;
+                    cnt++;
+                    Bfs(grid,i,j,n,m);
                 }
             }
         }
 
-
-        return count;
+        return cnt;
     }
 };
