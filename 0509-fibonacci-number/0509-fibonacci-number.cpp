dedbdac3492277a35aa8b594288d97a3813e5648
Harshit Vashisht <150457097+Vharshit03@users.noreply.github.com>
2026-06-03 18:36:40 +0530

    Time: 3 ms (62.2%), Space: 7.6 MB (88.15%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int fib(int n) {
+
+        if(n==0)
+        return 0;
+
+        int first=0,second=1,third=1;
+
+        for(int i=2;i<=n;i++){
+            third = first + second;
+            first = second;
+            second = third;
+        }
+
+        return third;
+        
+    }
+};
