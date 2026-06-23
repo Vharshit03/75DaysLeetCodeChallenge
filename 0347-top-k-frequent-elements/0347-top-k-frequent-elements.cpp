81933a877dfe6510ca3990e4fd8dc90822bcb394
Harshit Vashisht <150457097+Vharshit03@users.noreply.github.com>
2026-06-24 00:00:37 +0530

    Time: 0 ms (100%), Space: 18.1 MB (31.31%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
-    struct sortbysec{
-        bool operator()(pair<int,int> a,pair<int,int> b){
-        return a.second>b.second;
-        }
-    };
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
-        unordered_map<int,int>mp;
+        unordered_map<int,int> mp;
+        for(auto it:nums) mp[it]++;
 
-        for(auto n:nums){
-            mp[n]++;
+        vector<pair<int,int>> freq(mp.begin(),mp.end());
+        sort(freq.begin(),freq.end(),[](auto &a,auto &b) {
+            return b.second < a.second;
+        });
+
+        vector<int> result;
+
+        for(int i=0;i<k;i++){
+            result.push_back(freq[i].first);
         }
 
-
-        priority_queue<pair<int,int>,vector<pair<int,int>>,sortbysec> minheap;
-
-        for(auto m:mp){
-            minheap.push({m.first,m.second});
-
-            if(minheap.size()>k)
-            minheap.pop();
-        }
-
-        vector<int>ans;
-
-        while(!minheap.empty()){
-            ans.push_back(minheap.top().first);
-            minheap.pop();
-        }
-
-        return ans;
+        return result;
     }
 };
